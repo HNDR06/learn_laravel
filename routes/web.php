85cf636b7b0fae9c551bfef12f6df631a5f1f413
Hendrawan O <hendrawanoktavianto786@gmail.com>
2024-12-15 00:05:39 +0700

    Add Todo Controller and method post

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/todo', [TodoController::class,'index']);
+Route::post('/todo', [TodoController::class,'store']);
